a23512efcba2da12868afd171e9a62907f514b12
github username <github email address>
2019-08-10 23:15:07 -0500

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (tutorial) => {
+  return tutorial.toLowerCase().split(' ').map(word => {
+    return word[0].toUpperCase() + word.slice(1)
+  }).join(' ');
 }
+
+let newTutorials = tutorials.map(tutorial => {
+    return titleCased(tutorial)
+})
+
+// function titleCase(str) {
+//   return str.toLowerCase().split(' ').map(function(word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
